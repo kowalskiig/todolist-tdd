2f291d26839724ab9bd0456135bfa43c17e263fb
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-08-30 23:26:31 -0300

    feat(service): implementa método para criar tarefa no service

--- list/src/main/java/com/todo/list/service/TarefaServiceImpl.java
+++ list/src/main/java/com/todo/list/service/TarefaServiceImpl.java
@@ -2,11 +2,17 @@ package com.todo.list.service;
 
 import com.todo.list.dto.TarefaRequestDTO;
 import com.todo.list.dto.TarefaResponseDTO;
+import com.todo.list.repository.TarefaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TarefaServiceImpl implements TarefaServiceContrato {
 
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaServiceImpl(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
 
     @Override
     public TarefaResponseDTO criarTarefa(TarefaRequestDTO tarefaRequestDTO) {
