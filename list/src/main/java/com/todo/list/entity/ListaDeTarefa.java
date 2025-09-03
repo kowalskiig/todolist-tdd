d1bc74fe2cba8afc9f979fef2c6bf2a7c6c9d7fd
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-02 21:14:24 -0300

    feat(ListaDeTarefa): adiciona entidade ListaDeTarefa

--- list/src/main/java/com/todo/list/entity/ListaDeTarefa.java
+++ list/src/main/java/com/todo/list/entity/ListaDeTarefa.java
@@ -1,4 +1,43 @@
 package com.todo.list.entity;
 
+
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_lista_tarefas")
 public class ListaDeTarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(id.listaDeTarefa)
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+    public ListaDeTarefa(){}
+
+
+    public ListaDeTarefa(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
